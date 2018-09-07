59fa22472e33f08c19625b68fb00085767e4ca7c
Iain Holder <github@tech-nous.co.uk>
2018-09-07 16:41:55 +0100

    Added (non) logging.

---
+++ BitcoinVanityAddressFinder/Logging.cs
@@ -0,0 +1,7 @@
+﻿namespace BitcoinVanityAddressFinder
+{ // ReSharper disable once UnusedMember.Global
+    public class Logging
+    {
+        // Intentionally no logging in this app as it could be a security risk if private keys are languishing on disk.
+    }
+}
